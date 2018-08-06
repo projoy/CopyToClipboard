eae44a8cf41606d9fcb278ad4fea10fba9c8f29d
lijiyong3 <lijiyong@jd.com>
2018-08-06 11:17:15 +0800

    chrome插件

---
+++ plugin/js/content-script.js
@@ -0,0 +1,28 @@
+﻿console.log('content script!');
+
+// 注意，必须设置了run_at=document_start 此段代码才会生效
+document.addEventListener('DOMContentLoaded', function()
+{
+    console.log(location.host);
+
+    //根据站点，将相应的内容复制到粘贴板
+    if(location.host == "blog.csdn.net"){
+        var txtContent = $(".blog-content-box:first-child").html();
+ 
+        copyToClipboard(txtContent);
+    }   
+ 
+});
+
+
+function copyToClipboard( text ){
+                var copyDiv = document.createElement('div');
+                copyDiv.contentEditable = true;
+                document.body.appendChild(copyDiv);
+                copyDiv.innerHTML = text;
+                copyDiv.unselectable = "off";
+                copyDiv.focus();
+                document.execCommand('SelectAll');
+                document.execCommand("Copy", false, null);
+                document.body.removeChild(copyDiv);
+            }
